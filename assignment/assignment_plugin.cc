a9a9accc86daaffd1b49e24f2ba2b8e4ba50515b
Titan Yuan <tryuan99@gmail.com>
2025-02-25 16:52:09 -0800

    Add weighted even assignment to the plugin

--- assignment/assignment_plugin.cc
+++ assignment/assignment_plugin.cc
@@ -4,6 +4,7 @@
 
 #include "assignment/cover_assignment.h"
 #include "assignment/even_assignment.h"
+#include "assignment/weighted_even_assignment.h"
 
 namespace {
 // Create the cost matrix.
@@ -50,6 +51,31 @@ int Assignment_EvenAssignment_Assign(const int num_agents, const int num_tasks,
 
   // Perform the assignment.
   assignment::EvenAssignment assignment(num_agents, num_tasks, cost_matrix);
+  const auto assignments = assignment.Assign();
+
+  // Record the assignments.
+  for (int i = 0; i < assignments.size(); ++i) {
+    assigned_agents[i] = assignments[i].first;
+    assigned_tasks[i] = assignments[i].second;
+  }
+  return assignments.size();
+}
+
+// Assign the agents to the tasks using a weighted even assignment.
+int Assignment_WeightedEvenAssignment_Assign(
+    const int num_agents, const int num_tasks, const float* costs,
+    const float* weights, const int weight_scaling_factor, int* assigned_agents,
+    int* assigned_tasks) {
+  // Create the cost matrix and the task weights.
+  const auto cost_matrix = CreateCostMatrix(num_agents, num_tasks, costs);
+  std::vector<double> task_weights(num_tasks);
+  for (int i = 0; i < num_tasks; ++i) {
+    task_weights[i] = weights[i];
+  }
+
+  // Perform the assignment.
+  assignment::WeightedEvenAssignment assignment(
+      num_agents, num_tasks, cost_matrix, task_weights, weight_scaling_factor);
   const auto assignments = assignment.Assign();
 
   // Record the assignments.
